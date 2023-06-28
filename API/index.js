51019b9a3f69e6b490246d7cf2a50dce0ec2df6e
MML711 <mussiemekonnen7@gmail.com>
2023-06-28 12:20:24 +0200

    updated cors configuration

--- API/index.js
+++ API/index.js
@@ -12,25 +12,27 @@ const PORT = process.env.PORT || 4001;
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
-//middleware to handle any CORS issues
-app.use(cors({
-  origin: "*",
-  methods: ["POST", "GET", "PUT", "DELETE", "PATCH"],
-  allowedHeaders: ["Origin", "X-Requested-With", "Content-Type", "Accept", "Authorization", "token"],
-}));
+// https://cors-anywhere.herokuapp.com/
 
-/* app.use((req, res, next) => {
+app.use((req, res, next) => {
   res.header("Access-Control-Allow-Origin", "*");
   res.header(
     "Access-Control-Allow-Headers",
     "Origin, X-Requested-With, Content-Type, Accept, Authorization, token"
-  );
-  if (req.method === "options") {
+    );
+  if (req.method == "OPTIONS") {
     res.header("Access-Control-Allow-Methods", "POST, GET, PUT, DELETE, PATCH");
     return res.status(200).json({});
   }
   next();
-}); */
+});
+
+//middleware to handle any CORS issues
+app.use(cors({
+  origin: "*",
+  methods: `POST,GET,PUT,DELETE,PATCH`,
+  allowedHeaders: `Origin,X-Requested-With,Content-Type,Accept,Authorization,token`,
+}));
 
 app.use("/api", routes);
 
